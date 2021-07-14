412130f92c5f631a0c35b76fe7ec821c44c86f14
abondarevskii <bondarevskii.frontend@gmail.com>
2021-07-14 15:24:29 +0300

    added clients-slider

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,15 @@ $(function () {
       dots: true,
       arrows: false,
       fade: true
-   });
+   }),
+
+      $('.clients-slider__items').slick({
+         arrows: false,
+         autoplay: true,
+         speed: 3000,
+         slidesToShow: 5
+
+      });
 
    var mixer = mixitup('.products__items');
    var mixer = mixitup('.design__items');
